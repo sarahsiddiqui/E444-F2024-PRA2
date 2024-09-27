8e4ef075058da37c6f1fa5669d6084d4f0a9dd62
Sarah Siddiqui <sarahsiddiqui931@gmail.com>
2024-09-26 22:53:57 -0400

    Completed activity 1.2b

--- app/examples/app.py
+++ app/examples/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 def index():
     print("Something ran")
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {}!</h1>'.format(name)
