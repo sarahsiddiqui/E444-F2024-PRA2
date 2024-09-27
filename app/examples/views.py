38a689397f9455d5234bfc24363f1c872d814ed3
Sarah Siddiqui <sarahsiddiqui931@gmail.com>
2024-09-27 15:42:43 -0400

    Prints the current time

--- app/examples/views.py
+++ app/examples/views.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
-Bootstrap(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 app = Flask(__name__)
 @app.route('/')
@@ -12,5 +15,6 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    print("This printed")
-    return render_template('user.html', name=name)
+    currentDate = datetime.now()
+    timeString = currentDate.strftime("The local date and time is %A, %B %d, %Y %I:%M %p")
+    return render_template('user.html', name=name, current_time=timeString)
