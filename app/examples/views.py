b17514f26940e2d833446b7313f0ff82ffed83ee
Sarah Siddiqui <sarahsiddiqui931@gmail.com>
2024-09-26 23:28:16 -0400

    Now uses render_templates

--- app/examples/views.py
+++ app/examples/views.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # from . import hello
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    print("Something ran")
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
