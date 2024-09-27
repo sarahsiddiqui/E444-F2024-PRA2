9681d616ccb972bc771c3d9b4a09cbc8e961d1bb
Sarah Siddiqui <sarahsiddiqui931@gmail.com>
2024-09-26 23:15:47 -0400

    Removed app.py file, now using views.py

--- app/examples/views.py
+++ app/examples/views.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def index():
     print("Something ran")
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
